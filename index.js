c4c7193e1ec1fa008e9b49b5a755192142a26145
ElminAkberov <ekberovelmin04@gmail.com>
2024-08-12 13:39:52 +0400

    com

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-import express from 'express';
-import fs from 'fs';
-import path from 'path';
-
-const app = express();
-const port = 9000;
-
-// db.json dosyasının yolu
-const dbPath = path.resolve(__dirname, 'db.json');
-
-// Verileri oku ve JSON olarak döndür
-app.get('/', (req, res) => {
-  fs.readFile(dbPath, 'utf8', (err, data) => {
-    if (err) {
-      return res.status(500).json({ error: 'Veri okunamadı' });
-    }
-    res.json(JSON.parse(data));
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Starting Server on Port ${port}`);
-});
+import express from "express"
+const app = express()
+const port = 9000
+app.use("/",(req,res)=>{
+    res.json({message:"Hellos"})
+})
+app.listen(9000,()=>{
+    console.log(`Starting Server on Port ${port}`)
+})
